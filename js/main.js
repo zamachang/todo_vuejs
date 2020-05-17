355b3319dc7a6d08d4cc706d284ff3491e220535
zamachang <fujipurati+hogehoge@gmail.com>
2020-05-17 12:37:16 +0900

    完成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 var app = new Vue ({
-  el: '#app'
-
-  
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {gi
+      if(this.newItem == '') return;
+      var todo = {
+        item: this.newItem,
+        isDone: false
+      };
+      this.todos.push(todo);
+      this.newItem = '';
+    },
+    deleteItem: function(index) {
+      this.todos.splice(index, 1)
+    }
+  }
 })
 
